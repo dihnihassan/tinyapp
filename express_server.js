944237e87b96f435f994c38cc03c1e63e5b3f908
Dihni Hassan <dihnihassan@gmail.com>
2022-11-28 18:52:15 -0500

    Added urls_index

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -20,3 +24,9 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
+
+
+
