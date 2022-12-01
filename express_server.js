57516c717cb28bcc380e1f2f86ec9d4f7e7024a6
Dihni Hassan <dihnihassan@gmail.com>
2022-12-01 16:22:16 -0500

    added new urlDatabase object and return relevant errors for post /urls/:id

--- express_server.js
+++ express_server.js
@@ -39,6 +39,18 @@ const getUserFromReqs = function(req) {
   return user;
 }
 
+const urlsForUser = function(id) {
+  let urlResults = {};
+  for (const urls in urlDatabase) {
+    // console.log("This is for user URLS", urlDatabase[urls]);
+    if (id === urlDatabase[urls].userID) {
+      urlResults[urls] = urlDatabase[urls];
+    }
+
+  }console.log(urlResults);
+  return urlResults;
+}
+
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const { request } = require("express");
@@ -46,11 +58,21 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
 
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -59,9 +81,15 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls", (req, res) => {
+const user = getUserFromReqs(req);
+  if (!user) {
+    res.send ("Login or Register to view URLs")
+    return
+  }
+  const userUrls = urlsForUser(req.cookies.user_id);
   const templateVars = {
-    urls: urlDatabase,
-    user: users[req.cookies.user_id]
+    urls: userUrls,
+    user: users[req.cookies.user_id],
   };
   res.render("urls_index", templateVars);
 });
@@ -80,8 +108,15 @@ app.get("/urls/new", (req, res) => {
 });
 app.get("/urls/:id", (req, res) => {
   console.log(req.params.id);
+  const user = getUserFromReqs(req);
+  if (!user) {
+    res.redirect("/login")
+    return
+  } 
+  const longURL = urlDatabase[req.params.id].longURL;
   const templateVars = {
-    id: req.params.id, longURL: urlDatabase[req.params.id],
+    id: req.params.id, 
+    longURL: longURL,
     user: users[req.cookies.user_id]
   };
   res.render("urls_show", templateVars);
@@ -99,8 +134,11 @@ app.post("/urls", (req, res) => {
     return
   } 
   let randomID = generateRandomString(6);
-  let longUrl = req.body.longURL;
-  urlDatabase[randomID] = longUrl;
+  let longURL = req.body.longURL;
+  let userID = req.cookies.user_id;
+  urlDatabase[randomID] = {longURL, userID};
+  // console.log(urlDatabase[randomID]);
+  console.log({longURL, userID});
   // res.send("Ok"); 
   res.redirect(`/urls/${randomID}`);
 });
@@ -121,7 +159,7 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls")
 })
 app.post("/urls/:id/", (req, res) => {
-  urlDatabase[req.params.id] = req.body.newURL;
+  urlDatabase[req.params.id].longURL = req.body.newURL;
   res.redirect("/urls")
 })
 app.post("/login", (req, res) => {
